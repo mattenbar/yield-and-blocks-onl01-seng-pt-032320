4a4901e87fce681b9ef7e10e4f4d1bb0f43987ff
matthew enbar <matthew.enbar@gmail.com>
2020-03-25 22:58:21 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -9,8 +9,10 @@ array
 end
 # call your method here!
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
+hello_t(array = ["Tim", "Tom", "Jim"]) do |name|
+  if array.empty?
+    puts "hey you didnt pass a block"
+  elsif name.start_with?("T")
     puts "Hi, #{name}"
   end
 end
